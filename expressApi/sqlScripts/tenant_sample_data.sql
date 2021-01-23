c7daffef0f33fe1d6408fab8e1b74634a45e0c86
AdmiralKid <siddhharth47@gmail.com>
2021-01-23 22:10:07 +0530

    small change in smaple data.

--- expressApi/sqlScripts/tenant_sample_data.sql
+++ expressApi/sqlScripts/tenant_sample_data.sql
@@ -1,4 +1,4 @@
 insert into tenant (tenantname, tenantaddress, tenantmobilenumber, rentalagreementid, checkindate, checkoutdate, rentamount, flatid)
-VALUES ('Siddharth', 'address1', 1234567890, 1, '2020-12-27', null, 10000,101);
+VALUES ('Siddharth', '24, 3rd cross, Marrappa Garden, Benson Town, Bangalore: 560046', 1234567890, 1, '2020-12-27', null, 10000,101);
 insert into tenant (tenantname, tenantaddress, tenantmobilenumber, rentalagreementid, checkindate, checkoutdate, rentamount, flatid)
 VALUES ('Virab', 'address2', 1234567892, 2, '2020-12-27', null, 20000,201);
