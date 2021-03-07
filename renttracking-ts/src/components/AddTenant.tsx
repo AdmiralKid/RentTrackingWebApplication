c6ce55cd8dde80cab892cc99792ac5a392ad613c
AdmiralKid <siddhharth47@gmail.com>
2021-03-07 21:15:58 +0530

    Tenant Form Validation.

--- renttracking-ts/src/components/AddTenant.tsx
+++ renttracking-ts/src/components/AddTenant.tsx
@@ -9,63 +9,81 @@ interface propsType {
     onAddTenantModalSubmit: (tenant: Tenant)=>void;
 }
 function AddTenantModal(props:propsType) {
-    const [tenant, setTenant] = useState<Tenant>({tenantmobilenumber:0, tenantname:"", tenantaddress:"", tenantid:0,checkindate:new Date(),checkoutdate:new Date(), rentamount:0, flatid:0,rentalagreementid:0});
-    const handleTenantNameChange = (value: string) => {
-        var updatedTenant = tenant;
-        updatedTenant.tenantname = value;
-        setTenant(updatedTenant)
+    const [tenant, setTenant] = useState<Tenant>({tenantmobilenumber : 0, tenantname : "", tenantaddress : "", tenantid : 0, checkindate : new Date(), checkoutdate : new Date(), rentamount : 0, flatid : 0, rentalagreementid : 0});
+    const [errors, seterrors] = useState({tenantname : "", tenantaddress : "", tenantmobilenumber : ""});
+    const onModalCloseHandler = () => {
+        seterrors({tenantname : "", tenantaddress : "", tenantmobilenumber : ""});
+        props.closeModalFunc();
     }
-    const handleTenantAddressChange = (value: string) => {
-        var updatedTenant = tenant;
-        updatedTenant.tenantaddress = value;
-        setTenant(updatedTenant)
+    const handleChange = (event : any) => {
+        setTenant({...tenant, [event.target.name]:event.target.value});
     }
-    const handleTenantMobileNumberChange = (value: string) => {
-        var updatedTenant = tenant;
-        updatedTenant.tenantmobilenumber = +value;
-        setTenant(updatedTenant)
+    const validateForm = (tenantdata : Tenant) => {
+        let validErrors = {tenantname : "", tenantaddress : "", tenantmobilenumber : ""};
+        if(!tenantdata.tenantname.trim()){
+            validErrors.tenantname = "Tenant Name Required.";
+        }
+        else if(tenantdata.tenantname.length<3){
+            validErrors.tenantname = "Tenant Name Must be at least 3 characters.";
+        }
+        if(!tenantdata.tenantaddress.trim()){
+            validErrors.tenantaddress = "Tenant Address Required.";
+        }
+        if(tenantdata.tenantmobilenumber.toString().trim().length !== 10){
+            validErrors.tenantmobilenumber = "Invalid Mobile number. Remove 0  or +91";
+        }
+        return validErrors;
     }
+    const handleSubmit = (event : any) => {
+        event.preventDefault();
+        let validErrors = validateForm(tenant);
+        seterrors(validErrors);
+        if(validErrors.tenantname === "" && validErrors.tenantaddress === "" && validErrors.tenantmobilenumber === ""){
+            props.onAddTenantModalSubmit(tenant);
+            onModalCloseHandler();
+        }
+    }
+    
     return (
         <>
-            <Modal show={props.showModal} onHide={()=>{props.closeModalFunc()}}>
+            <Modal show={props.showModal} onHide={()=>{onModalCloseHandler()}}>
                 <Modal.Header closeButton>
                     <Modal.Title>Add New Tenant</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group controlId="formTenantName">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control placeholder="Tenant Name" onChange={(e)=>{handleTenantNameChange(e.target.value)}} />
-                            <Form.Control.Feedback type="invalid">
-                                Invalid
-                            </Form.Control.Feedback>
-                        </Form.Group>
+                <Form onSubmit={(e)=>{handleSubmit(e)}}>
+                    <Modal.Body>
+                        
+                            <Form.Group controlId="formTenantName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control name="tenantname" placeholder="Tenant Name" onChange={(e)=>{handleChange(e)}} isInvalid={!!errors.tenantname}/>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.tenantname}
+                                </Form.Control.Feedback>
+                            </Form.Group>
 
-                        <Form.Group controlId="formTenantAddress">
-                            <Form.Label>Address</Form.Label>
-                            <Form.Control as="textarea" onChange={(e)=>{handleTenantAddressChange(e.target.value)}} />
-                            <Form.Control.Feedback type="invalid">
-                            Invalid
-                            </Form.Control.Feedback>
-                        </Form.Group>
+                            <Form.Group controlId="formTenantAddress">
+                                <Form.Label>Address</Form.Label>
+                                <Form.Control name="tenantaddress" as="textarea" onChange={(e)=>{handleChange(e)}} isInvalid={!!errors.tenantaddress} />
+                                <Form.Control.Feedback type="invalid">
+                                {errors.tenantaddress}
+                                </Form.Control.Feedback>
+                            </Form.Group>
 
-                        <Form.Group controlId="formTenantMobileNumber">
-                            <Form.Label>Contact Number</Form.Label>
-                            <Form.Control type="number" onChange={(e)=>{handleTenantMobileNumberChange(e.target.value)}} />
-                            <Form.Control.Feedback type="invalid">
-                            Invalid
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={()=>{
-                            //props.onAddTenantModalSubmit(tenant);
-                            props.closeModalFunc();                  
-                    }}>
-                    Add
-                    </Button>
-                </Modal.Footer>
+                            <Form.Group controlId="formTenantMobileNumber">
+                                <Form.Label>Contact Number</Form.Label>
+                                <Form.Control name="tenantmobilenumber" type="number" onChange={(e)=>{handleChange(e)}} isInvalid={!!errors.tenantmobilenumber}/>
+                                <Form.Control.Feedback type="invalid">
+                                {errors.tenantmobilenumber}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" type="submit">
+                        Add
+                        </Button>
+                    </Modal.Footer>
+                </Form>
             </Modal>
         </>
     )
