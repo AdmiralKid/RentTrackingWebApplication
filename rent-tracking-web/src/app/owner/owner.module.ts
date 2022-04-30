27741db9d129d84300e51380cc43f58c4b3f9e90
Virab Shetkar <virabarunshetkar@gmail.com>
2022-04-30 14:02:02 +0530

    Removed Material Module. From now on create a MaterialImports inside feature modules and import Material modules

--- rent-tracking-web/src/app/owner/owner.module.ts
+++ rent-tracking-web/src/app/owner/owner.module.ts
@@ -3,13 +3,31 @@ import { CommonModule } from '@angular/common';
 import { OwnerComponent } from './owner.component';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { OwnerRoutingModule } from './owner-routing.module';
-import { MaterialModule } from '../core/material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ApartmentsComponent } from './containers/apartments/apartments.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ApartmentStoreModule } from '../core/store/apartment/apartment-store.module';
 import { ApartmentListComponent } from './components/apartment-list/apartment-list.component';
 import { ApartmentFormComponent } from './components/apartment-form/apartment-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+const MaterialImports = [
+  MatButtonModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatTabsModule,
+  MatMenuModule,
+  MatCardModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [
@@ -23,9 +41,9 @@ import { ApartmentFormComponent } from './components/apartment-form/apartment-fo
   imports: [
     CommonModule,
     OwnerRoutingModule,
-    MaterialModule,
     ReactiveFormsModule,
     ApartmentStoreModule,
+    MaterialImports,
   ],
 })
 export class OwnerModule {}
