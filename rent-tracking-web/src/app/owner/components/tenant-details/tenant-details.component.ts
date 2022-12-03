0df0de74bdc00b8ec942a0262537e042d06809e3
Virab Shetkar <virabarunshetkar@gmail.com>
2022-12-03 12:25:27 +0530

    Added styling for Flat page

--- rent-tracking-web/src/app/owner/components/tenant-details/tenant-details.component.ts
+++ rent-tracking-web/src/app/owner/components/tenant-details/tenant-details.component.ts
@@ -5,13 +5,12 @@ import { User } from '../../../core/models/user.model';
 @Component({
   selector: 'app-tenant-details',
   templateUrl: './tenant-details.component.html',
-  styleUrls: ['./tenant-details.component.scss']
+  styleUrls: ['./tenant-details.component.scss'],
 })
 export class TenantDetailsComponent implements OnInit {
-  @Input() tenant!: Observable<User| undefined>;
-  constructor() { }
+  @Input() tenant!: Observable<User | undefined>;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
